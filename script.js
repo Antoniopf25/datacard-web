eec5028bdf5de7ddba4ab549cb1540d742ffbda2
Antoniopf25 <58468303+Antoniopf25@users.noreply.github.com>
2025-06-07 22:07:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 
 function printPDF() {
-  const content = document.getElementById('print-area').cloneNode(true);
-  const win = window.open('', '', 'width=800,height=1000');
-  win.document.write('<html><head><title>Datacard</title>');
-  win.document.write('<link rel="stylesheet" href="style.css">');
-  win.document.write('</head><body>');
-  win.document.body.appendChild(content);
-  win.document.write('</body></html>');
-  win.document.close();
-  win.focus();
-  win.print();
-  win.close();
+  window.print();
 }
 
 function showPage(id) {
